d24cf4586ed483606e0e6fa5f3ecf9bfdcf464ff
Rafael Rodriguez <rrg1459@hotmail.com>
2020-07-30 06:38:48 -0400

    Refactorizando

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,53 +1,41 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
  
-  def index
-    @posts = Post.order('created_at DESC')
-  end
+	def index
+		@posts = Post.order('created_at DESC')
+	end
  
-  def show
-  end
+	def show
+	end
  
-  def new
-    @post = Post.new
-  end
+	def new
+		@post = Post.new
+	end
  
-  def create
-    @post = Post.new(post_params)
-    if @post.save
-      redirect_to posts_path, notice: 'Se creo el producto correctamente'
-    else
-      render :new
-    end
-  end
+	def create
+		@post = Post.new(post_params)
+		@post.save ? (redirect_to posts_path, notice: 'Producto creado') : (render :new)
+	end
  
-  def edit
-  end
+	def edit
+	end
  
-  def update
-    if @post.update_attributes(post_params)
-      redirect_to post_path(@post), notice: 'Se actualizo el producto correctamente'
-    else
-      render :edit
-    end
-  end
+	def update
+		@post.update_attributes(post_params) ? (redirect_to post_path(@post), notice: 'Producto actualizado') : (render :edit)
+	end
 
-  def destroy
-    if @post.destroy
-      mensaje = 'Se borro correctamente el producto'
-    else
-      mensaje = 'Se presento un problema al tratar de borrar este producto'
-    end
-    redirect_to root_url, notice: mensaje
-  end
+	def destroy
+		@post.destroy ? mensaje = 'Producto borrado' : mensaje = 'No se pudo borrar el producto'
+		redirect_to root_url, notice: mensaje
+	end
 
-  private
+	private
  
-  def post_params
-    params.require(:post).permit(:title, :body, :image, :remove_image, attachments: [])
-  end
+	def post_params
+		params.require(:post).permit(:title, :body, :image, :remove_image)
+	end
  
-  def set_post
-    @post = Post.find(params[:id])
-  end
+	def set_post
+		@post = Post.find(params[:id])
+	end
 end
